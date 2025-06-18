9422cdee690ce1e9751a991d610f9f6d73b95a2e
Raymond Julin <raymond.julin@gmail.com>
2025-06-18 20:53:55 +0200

    Update index.ts

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,12 @@
 import { drizzle } from 'drizzle-orm/node-postgres'
-import { Pool } from 'pg'
 import { config } from 'dotenv'
 
 config({ path: './.env' })
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL })
-export const db = drizzle(pool)
+export const db = drizzle({ 
+  connection: { 
+    connectionString: process.env.DATABASE_URL!,
+  }
+});
 
 export * from './schema'
