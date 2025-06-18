c0b09135f2d3f8e3fd28fc06d823af87882a0522
Raymond Julin <raymond.julin@gmail.com>
2025-06-18 21:23:31 +0200

    Update auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -1,7 +1,8 @@
 import { betterAuth } from 'better-auth'
 import { magicLink } from 'better-auth/plugins/magic-link'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
-import { db, users } from './db'
+import { db } from './db'
+import * as schema from '../auth-schema'
 import { config } from 'dotenv'
 import sgMail from '@sendgrid/mail'
 
@@ -12,7 +13,14 @@ sgMail.setApiKey(process.env.SENDGRID_API_KEY!)
 export const auth = betterAuth({
   baseURL: process.env.BASE_URL || 'http://localhost:3000',
   secret: process.env.AUTH_SECRET!,
-  database: drizzleAdapter(db, { schema: { users }, provider: 'pg', usePlural: true }),
+  database: drizzleAdapter(db, {
+    schema: {
+      ...schema,
+      user: schema.users
+    },
+    provider: 'pg',
+    usePlural: true
+  }),
   plugins: [
     magicLink({
       async sendMagicLink({ email, url }) {
